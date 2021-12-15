0f602041920ccf2da3119bb848d19568620b43d8
Ing. Raúl Adolfo Torres Vargas <ingrauladolfo.torresvargas@gmail.com>
2021-12-15 12:12:16 -0600

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+const selectedPizza = document.getElementById("sabor-pizza");
+const selectedPizzaContainer = document.querySelector(".pizza-seleccionada");
+const paymentArea = document.querySelector(".cuadro-pago");
+// Pizza selected flavour
+const quantityFlavourPizza = () => {
+  let image = document.createElement("img");
+};
+selectedPizza.addEventListener("change", quantityFlavourPizza);
